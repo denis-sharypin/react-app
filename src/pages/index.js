b662676c9c6372fd2bb7f8715cbe1cb41feda196
denis-sharypin <mailmegoodnews@gmail.com>
2018-11-01 12:45:16 +0100

    Change link

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to</Link>
+    <Link to="/page-2/">Go to page</Link>
   </Layout>
 )
 
