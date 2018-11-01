d8a9d52c7ecdf27753b994c8ca83872c6d3462c0
denis-sharypin <mailmegoodnews@gmail.com>
2018-11-01 12:29:24 +0100

    lol

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to design and code react apps</h1>
+    <p>Just trying.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page</Link>
+  </Layout>
+)
+
+export default IndexPage
